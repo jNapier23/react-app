625fa4e808397acfa5e2769a154efa556ac50a08
James Napier <james_napier@outlook.com>
2023-03-23 12:32:16 +0000

    Delete react-app directory

--- react-app/src/Message.tsx
+++
@@ -1,8 +0,0 @@
-function Message() {
-  //Below is not HTML, it is JSX: JavaScript XML
-  const name = "James";
-  if (name) return <h1>Hello {name}</h1>;
-  return <h1>Hello World</h1>;
-}
-
-export default Message;
